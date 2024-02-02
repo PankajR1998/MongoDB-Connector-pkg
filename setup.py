ebc55bd842eba5b580c6d2223faaf97dc965fdf0
Pankaj <rawatpankaj9919@gmail.com>
2024-02-02 12:04:17 +0530

    updated setup.py file

--- setup.py
+++ setup.py
@@ -36,8 +36,8 @@ setup(
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker":f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"
-    }
-    package_dir={"":"src"}
+    },
+    package_dir={"":"src"},
     packages=find_packages(where="src"),
     install_requires=get_requirements(Path('./requirements_dev.txt'))
 )   
