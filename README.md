398ddf588a841bf719b2294dac62b30578ba66c9
Pankaj <rawatpankaj9919@gmail.com>
2024-02-02 11:54:12 +0530

    everything updated

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# MongoDB-Connector-pkg" 
+
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages,setup
 from typing import List
-
-"""HYPEN_E_DOT='-e .'
+from pathlib import Path
+HYPEN_E_DOT='-e .'
 
 def get_requirements(file_path:str)->List[str]:
     requirements=[]
@@ -12,13 +12,32 @@ def get_requirements(file_path:str)->List[str]:
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
 
-    return requirements"""
+    return requirements
+
+with open('README.md','r',encoding='utf-8') as f:
+    long_description = f.read()
+
+__version__ = "0.0.4"
+REPO_NAME = "MongoDB-Connector-pkg"
+PKG_NAME = "MongDB-Connect" # by this name package will be visible.
+AUTHOR_USER_NAME = "PankajR1998"
+AUTHOR_EMAIL = "rawatpankaj9919@gmail.com"
+
+
 
 setup(
-    name='DimondPricePrediction',
-    version='0.0.1',
-    author='sunny savita',
-    author_email='sunny.savita@ineuron.ai',
-    install_requires=["scikit-learn","pandas","numpy"],
-    packages=find_packages()
-)
+    name=PKG_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A python package for connecting with database.",
+    long_description=long_description,
+    long_description_content="text/makedown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker":f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"
+    }
+    package_dir={"":"src"},
+    packages=find_packages(where="src"),
+    install_requires=get_requirements(Path('./requirements_dev.txt'))
+)   
