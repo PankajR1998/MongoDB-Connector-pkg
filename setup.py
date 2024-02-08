c38ca657785e6b57b4672ee64fe271df052fa9fe
Pankaj Rawat <105982660+PankajR1998@users.noreply.github.com>
2024-02-08 17:27:20 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,15 @@
-from setuptools import find_packages,setup
+from setuptools import setup, find_packages
 from typing import List
-from pathlib import Path
-HYPEN_E_DOT='-e .'
 
-def get_requirements(file_path:str)->List[str]:
-    requirements=[]
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
-
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-
-    return requirements
-
-with open('README.md','r',encoding='utf-8') as f:
-    long_description = f.read()
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()     
+   
 
 __version__ = "0.0.4"
 REPO_NAME = "MongoDB-Connector-pkg"
 PKG_NAME = "mongohelper" # by this name package will be visible.
 AUTHOR_USER_NAME = "PankajR1998"
 AUTHOR_EMAIL = "rawatpankaj9919@gmail.com"
-
-
 
 setup(
     name=PKG_NAME,
@@ -32,11 +18,16 @@ setup(
     author_email=AUTHOR_EMAIL,
     description="A python package for connecting with database.",
     long_description=long_description,
-    long_description_content="text/makedown",
+    long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
-        "Bug Tracker":f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    )
+
+
     package_dir={"":"src"},
     packages=find_packages(where="src"),
     # install_requires=get_requirements(Path('./requirements.txt'))
